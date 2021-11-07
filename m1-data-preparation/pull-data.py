acc765c050ea6cf1285cde97a929924efd7200c3
João Diogo Martins Romão <up201806779@fe.up.pt>
2021-11-07 12:41:35 +0000

    Not fetching repeated data

--- m1-data-preparation/pull-data.py
+++ m1-data-preparation/pull-data.py
@@ -22,7 +22,7 @@ def article_url(timestamp, url):
     return "https://arquivo.pt/noFrame/replay/{}/{}".format(timestamp, url)
 
 def domain_request(domain):
-    r = requests.get(cdx_url(2018, 2020, domain))
+    r = requests.get(cdx_url(2020, 2021, domain))
     if r.status_code != 200: return
 
     domain_dic = {}
@@ -40,13 +40,16 @@ def article_request(response, domain_dic):
         return
     if parsed["status"] != "200": return
     
-    url = article_url(parsed["timestamp"], parsed["url"])
-    print("[processing] url =", url)
-    r = requests.get(url)
-    if r.status_code != 200: return
-
     article_entry = domain_dic.get(parsed["urlkey"], [])
-    article_entry.append((parsed["timestamp"], r.text))
+    if article_entry:
+        print("[updating entry] key =", parsed["urlkey"])
+        article_entry[1].append(parsed["timestamp"])
+    else:
+        print("[getting data] key =", parsed["urlkey"])
+        url = article_url(parsed["timestamp"], parsed["url"])
+        r = requests.get(url)
+        if r.status_code != 200: return
+        article_entry = [r.text, [parsed["timestamp"]]]
     domain_dic[parsed["urlkey"]] = article_entry
 
 for domain in domains:
