c629fd9ffbf2603fd8a69c7eb4f0c81c161535db
rafaavc <rafavc2508@gmail.com>
2021-11-11 18:46:25 +0000

    Refactoring and performance improvements

--- m1-data-preparation/pull-data.py
+++ m1-data-preparation/pull-data.py
@@ -3,14 +3,30 @@ from tqdm import tqdm
 from bs4 import BeautifulSoup
 import requests, json, time, re, sys
 
+NOTICIAS_AO_MINUTO = "noticiasaominuto"
+JORNAL_DE_NEGOCIOS = "jornaldenegocios"
+EXAME_INFORMATICA = "exameinformatica"
+
 domains = {
-    "https://www.noticiasaominuto.com/tech": re.compile("^(com,noticiasaominuto\)\/tech\/[0-9]+\/[a-z0-9-%]+)(?:(?:\?|&).*)?$"),
-    "https://www.jornaldenegocios.pt/empresas/tecnologias": re.compile("^(pt,jornaldenegocios\)\/empresas\/tecnologias\/[a-z0-9-%]+\/[a-z0-9-%]+)(?:\?.*)?$"),
-    "https://visao.sapo.pt/exameinformatica/noticias-ei": re.compile("^(pt,sapo,visao\)\/exameinformatica\/noticias-ei(?:\/[a-z0-9-%]+)?\/[0-9]+-[a-z0-9-%.]+)(?:\?.*)?$")
+    NOTICIAS_AO_MINUTO: {
+        "regex": re.compile("^(com,noticiasaominuto\)\/tech\/[0-9]+\/[a-z0-9-%]+)(?:(?:\?|&).*)?$"),
+        "url": "https://www.noticiasaominuto.com/tech"
+    },
+    JORNAL_DE_NEGOCIOS: {
+        "url": "https://www.jornaldenegocios.pt/empresas/tecnologias",
+        "regex": re.compile("^(pt,jornaldenegocios\)\/empresas\/tecnologias\/[a-z0-9-%]+\/[a-z0-9-%]+)(?:\?.*)?$")
+    },
+    EXAME_INFORMATICA: {
+        "url": "https://visao.sapo.pt/exameinformatica/noticias-ei",
+        "regex": re.compile("^(pt,sapo,visao\)\/exameinformatica\/noticias-ei(?:\/[a-z0-9-%]+)?\/[0-9]+-[a-z0-9-%.]+)(?:\?.*)?$")
+    }
 }
 '''
 domains = {
-    domain_url: article_urlkey_verification_regex (regex to verify if urlkey belongs to an article and to capture the part of the url without get parameters)
+    id: {
+        url
+        regex (regex to verify if urlkey belongs to an article and to capture the part of the url without get parameters)
+    }
 }
 '''
 
@@ -31,10 +47,10 @@ def cdx_url(date_from, date_to, url):
 def article_url(timestamp, url):
     return "https://arquivo.pt/noFrame/replay/{}/{}".format(timestamp, url)
 
-def domain_request(domain, article_validation_regex):
-    print(f"\nRequesting CDX data for {domain}...")
+def domain_request(domain_id, domain_data):
+    print(f"\nRequesting CDX data for {domain_id}...")
 
-    r = requests.get(cdx_url(20210101000000, 20211101000000, domain))
+    r = requests.get(cdx_url(20210101000000, 20211101000000, domain_data["url"]))
     if r.status_code != 200: return
 
     domain_dic = {}
@@ -45,16 +61,16 @@ def domain_request(domain, article_validation_regex):
     progress_bar = tqdm(total=len(data), file=sys.stdout)
 
     with ThreadPoolExecutor(max_workers=8) as executor:
-        executor.map(lambda cdx_entry: article_request(cdx_entry, domain_dic, article_validation_regex, progress_bar), data)
+        executor.map(lambda cdx_entry: article_request(cdx_entry, domain_dic, domain_id, domain_data["regex"], progress_bar), data)
 
     progress_bar.close()
-    d[domain] = domain_dic
+    d[domain_id] = domain_dic
 
 dates_regex = {
-    "noticiasaominuto" : re.compile("(\d\d):(\d\d) - (\d\d)\/(\d\d)\/(\d\d)"),
-    "jornaldenegocios" : re.compile("(\d\d) de (.*?) de (\d\d\d\d) às (\d\d):(\d\d)"),
-    "jornaldenegocios_only_hour" : re.compile("(\d\d):(\d\d)"),
-    "exameinformatica" : re.compile("(\d\d)\.(\d\d)\.(\d\d\d\d) às (\d\d)h(\d\d)")
+    NOTICIAS_AO_MINUTO : re.compile("(\d\d):(\d\d) - (\d\d)\/(\d\d)\/(\d\d)"),
+    JORNAL_DE_NEGOCIOS : re.compile("(\d\d) de (.*?) de (\d\d\d\d) às (\d\d):(\d\d)"),
+    JORNAL_DE_NEGOCIOS + "_only_hour" : re.compile("(\d\d):(\d\d)"),
+    EXAME_INFORMATICA : re.compile("(\d\d)\.(\d\d)\.(\d\d\d\d) às (\d\d)h(\d\d)")
 }
 
 newline_replace_regex = re.compile("\r?\n ?(?:\r?\n ?)+")
@@ -77,10 +93,10 @@ month_encoding = {
 def parse_date(indexation_timestamp, newspaper, date_string):
     match = dates_regex[newspaper].match(date_string)
 
-    if newspaper == "noticiasaominuto" and match:
+    if newspaper == NOTICIAS_AO_MINUTO and match:
         return "20" + match.group(5) + match.group(4) + match.group(3) + match.group(1) + match.group(2) + "00"
     
-    elif newspaper == "jornaldenegocios":
+    elif newspaper == JORNAL_DE_NEGOCIOS:
         if match:
             return match.group(3) + month_encoding[match.group(2).lower()] + match.group(1) + match.group(4) + match.group(5) + "00"
         
@@ -88,29 +104,28 @@ def parse_date(indexation_timestamp, newspaper, date_string):
         if hour_match:
             return indexation_timestamp[:8] + hour_match.group(1) + hour_match.group(2) + "00"
     
-    elif newspaper == "exameinformatica" and match:
+    elif newspaper == EXAME_INFORMATICA and match:
         return match.group(3) + match.group(2) + match.group(1) + match.group(4) + match.group(5) + "00"
     
     return ""
 
 
-def get_article_info_bs(url : str, indexation_timestamp, html = None):
+def get_article_info(domain_id, indexation_timestamp, html = None):
     if not html: return
 
     soup = BeautifulSoup(html, "html.parser")
 
-    info = None
-    if re.search("exameinformatica", url):
+    if EXAME_INFORMATICA == domain_id:
         authors = soup.select_one(".author-meta .name")
         
-        date = parse_date(indexation_timestamp, "exameinformatica", soup.select_one(".publish-date").get_text())
+        date = parse_date(indexation_timestamp, EXAME_INFORMATICA, soup.select_one(".publish-date").get_text())
         
         image = soup.select_one(".wp-block-image img")
         if image == None: image = soup.select_one(".thumbnail-image-wrapper img")
 
         if image != None and "src" not in image.attrs and "data-src" in image.attrs: image.attrs["src"] = image.attrs["data-src"]
 
-        info = {
+        return {
             'title' : soup.select_one(".entry-title").get_text(),
             'summary' : soup.select_one(".entry-excerpt").get_text().strip("\n "),
             'image' : image.attrs['src'] if image != None else "",
@@ -119,12 +134,12 @@ def get_article_info_bs(url : str, indexation_timestamp, html = None):
             'text' : newline_replace_regex.sub("\n", soup.select_one(".entry-content").get_text().strip("\r\n"))
         }
 
-    elif re.search("noticiasaominuto", url):
+    if NOTICIAS_AO_MINUTO == domain_id:
         authors = soup.select_one(".author-hover")
 
-        date = parse_date(indexation_timestamp, "noticiasaominuto", soup.select_one(".news-info-time").contents[0])
+        date = parse_date(indexation_timestamp, NOTICIAS_AO_MINUTO, soup.select_one(".news-info-time").contents[0])
 
-        info = {
+        return {
             'title' : soup.select_one(".news-headline").get_text(),
             'summary' : soup.select_one(".news-subheadline").get_text(),
             'image' : soup.select_one(".news-main-image picture img").attrs['src'],
@@ -133,12 +148,12 @@ def get_article_info_bs(url : str, indexation_timestamp, html = None):
             'text' : newline_replace_regex.sub("\n", soup.select_one(".news-main-text").get_text().strip("\r\n"))
         }
     
-    elif re.search("jornaldenegocios", url):
+    if JORNAL_DE_NEGOCIOS == domain_id:
         authors = soup.select_one(".info_autor strong")
         
-        date = parse_date(indexation_timestamp, "jornaldenegocios", soup.select_one(".info_autor span").get_text())
+        date = parse_date(indexation_timestamp, JORNAL_DE_NEGOCIOS, soup.select_one(".info_autor span").get_text())
         
-        info = {
+        return {
             'title' : soup.select_one(".article_title").get_text().strip(),
             'summary' : soup.select_one(".lead").get_text().strip(),
             'image' : soup.select_one(".multimedia_container img").attrs['src'],
@@ -147,62 +162,49 @@ def get_article_info_bs(url : str, indexation_timestamp, html = None):
             'text' : newline_replace_regex.sub("\n", soup.select_one(".main_text .texto").get_text().strip("\r\n"))
         }
 
-    
+    return None
 
-    return info
-
-errors = []
-
-def article_request(response, domain_dic, article_validation_regex, progress_bar):
+def article_request(response, domain_dic, domain_id, article_validation_regex, progress_bar):
+    progress_bar.update(1)
     try:
-        progress_bar.update(1)
-        try:
-            parsed = json.loads(response)
-        except Exception:
-            print(f"Invalid JSON string: {response}", file=sys.stderr)
-            return
-        if parsed["status"] != "200": return
+        parsed = json.loads(response)
+    except Exception:
+        print(f"Invalid JSON string: {response}", file=sys.stderr)
+        return
+    if parsed["status"] != "200": return
 
-        # This checks if the entry is an article and captures the first part of the urlkey (without GET parameters)
-        urlkey_match = article_validation_regex.match(parsed["urlkey"])
-        if not urlkey_match:
-            print(f"NOT an article: {parsed['urlkey']}", file=sys.stderr)
-            return
+    # This checks if the entry is an article and captures the first part of the urlkey (without GET parameters)
+    urlkey_match = article_validation_regex.match(parsed["urlkey"])
+    if not urlkey_match:
+        print(f"NOT an article: {parsed['urlkey']}", file=sys.stderr)
+        return
 
-        urlkey_base = urlkey_match.group(1) # urlkey without get parameters
+    urlkey_base = urlkey_match.group(1) # urlkey without get parameters
+    
+    article_entry = domain_dic.get(urlkey_base, {})
+    url = article_url(parsed["timestamp"], parsed["url"])
+
+    r = None
+    while True:
+        first = r == None
+        r = requests.get(url)
         
-        article_entry = domain_dic.get(urlkey_base, {})
-        url = article_url(parsed["timestamp"], parsed["url"])
+        if r.status_code == 200: break
+        elif r.status_code == 429:
+            if first: print("Threshold reached.", file=sys.stderr)
+            time.sleep(10)
+        else:
+            print(f"Invalid url: {url}, status code: {r.status_code}", file=sys.stderr)
+            return
 
-        r = None
-        while True:
-            first = r == None
-            r = requests.get(url)
-            
-            if r.status_code == 200: break
-            elif r.status_code == 429:
-                if first: print("Threshold reached.", file=sys.stderr)
-                time.sleep(10)
-            else:
-                print(f"Invalid url: {url}, status code: {r.status_code}", file=sys.stderr)
-                return
+    parsed["article"] = get_article_info(domain_id, parsed["timestamp"], r.text)
 
-        # Running newspaper3k on the received HTML
-        parsed["article"] = get_article_info_bs(url, parsed["timestamp"], r.text)
-
-        article_entry[parsed["timestamp"]] = parsed
-        domain_dic[urlkey_base] = article_entry
-    except Exception as e:
-        errors.append(f"{response}:\n{str(e)}")
+    article_entry[parsed["timestamp"]] = parsed
+    domain_dic[urlkey_base] = article_entry
 
 print("Starting data retrieval.")
-for domain, regex in domains.items():
-    domain_request(domain, regex)
-
-if len(errors):
-    print("There have been thread exceptions!")
-    print("!! THREAD EXCEPTIONS", file=sys.stderr)
-    for e in errors: print(e, file=sys.stderr)
+for domain_id, domain_data in domains.items():
+    domain_request(domain_id, domain_data)
 
 with open("data.json", "w") as file:
     json.dump(d, file, indent=4)
