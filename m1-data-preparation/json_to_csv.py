b728c143fee76df690feb9ef1d3f69f9ea484beb
rafaavc <rafavc2508@gmail.com>
2021-11-15 11:37:53 +0000

    Added news_entities relaation to json_to_csv

--- m1-data-preparation/json_to_csv.py
+++ m1-data-preparation/json_to_csv.py
@@ -6,7 +6,10 @@ def check_args():
         return False
     return True
 
+news_entity_relation_csv = ["news_pk,entity_pk\n"]
+
 def flatten_json(data):
+    global news_entity_relation_csv
     domains = [["domain_pk", "domain"]]
     urlkeys = [["urlkey_pk", "domain_pk", "urlkey"]]
     news = [["news_pk", "urlkey_pk", "timestamp", "title", "summary", "image", "publish_date", "authors", "text"]]
@@ -27,6 +30,8 @@ def flatten_json(data):
             for timestamp in timestamps.keys():
                 element = [news_pk, urlkey_pk, timestamp]
                 article = timestamps[timestamp]["article"]
+                for entity in article["entities"]:
+                    news_entity_relation_csv.append(f"{news_pk},{entity}\n")
                 article_details = [article[key] for key in article if key != "entities"] # we don't want to include the entities in the csv
                 element.extend(article_details)
                 news.append(element)
@@ -62,3 +67,5 @@ def main():
                 writer.writerow(row)
 
 main()
+with open("csv/news_entities.csv", "w") as f:
+    f.writelines(news_entity_relation_csv)
