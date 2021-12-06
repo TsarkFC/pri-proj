954c04a95b06b87a14446d85b4e210f39e2f0f2f
rafaavc <rafavc2508@gmail.com>
2021-12-06 12:28:16 +0000

    Resolving entities references

--- m2-information-retrieval/scripts/get_solr_data.py
+++ m2-information-retrieval/scripts/get_solr_data.py
@@ -13,11 +13,12 @@ output = []
 def format_date_to_solr(original):
     # YYYYMMDDhhmmss to YYYY-MM-DDThh:mm:ssZ
     if original == "":
-        print("putting empty")
         return None
     return original[:4] + "-" + original[4:6] + "-" + original[6:8] + "T" + original[8:10] + ":" + original[10:12] + ":" + original[12:14] + "Z"
 
-# entities = pd.read_csv("./dataset/entities.csv", sep=",")
+entities = pd.read_csv("./dataset/entities.csv", sep=",")
+
+entities_memo = {}
 
 for newspaper in data.keys():
     for urlkey in data[newspaper]:
@@ -27,10 +28,17 @@ for newspaper in data.keys():
             obj["newspaper"] = newspaper
             obj["timestamp"] = format_date_to_solr(obj["timestamp"])
             obj["article"]["publish_date"] = format_date_to_solr(obj["article"]["publish_date"])
-            # new_entities = []
-            # for entity_id in obj["article"]["entities"]:
-            #     line = entities[entities["entity_pk"] == entity_id]
-            #     print(line)
+            new_entities = []
+            for entity_id in obj["article"]["entities"]:
+                if (entity_id in entities_memo):
+                    new_entities.append(entities_memo[entity_id])
+                else:
+                    line = entities[entities["entity_pk"] == entity_id]
+                    parsed = line.to_dict(orient="records")[0]
+                    new_entities.append(parsed)
+                    entities_memo[entity_id] = parsed
+
+            obj["article"]["entities"] = new_entities
             output.append(obj)
 
 json.dump(output, output_file, indent=4)
