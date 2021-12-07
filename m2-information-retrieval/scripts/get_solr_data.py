7e6b15fb6fb9c702493177208f79678ebb5d2ed7
rafaavc <rafavc2508@gmail.com>
2021-12-07 16:34:50 +0000

    Improving data, schema, and execution

--- m2-information-retrieval/scripts/get_solr_data.py
+++ m2-information-retrieval/scripts/get_solr_data.py
@@ -19,11 +19,13 @@ def format_date_to_solr(original):
 entities = pd.read_csv("./dataset/entities.csv", sep=",")
 
 entities_memo = {}
+fields_to_remove = ["mime", "status", "digest", "length", "offset", "filename", "collection", "source", "source-coll"]
 
 for newspaper in data.keys():
     for urlkey in data[newspaper]:
         for version in data[newspaper][urlkey]:
             obj = data[newspaper][urlkey][version]
+            for field in fields_to_remove: del(obj[field])
             obj["urlkey"] = urlkey # update urlkey - some versions have different urlkeys that have query parameters
             obj["newspaper"] = newspaper
             obj["timestamp"] = format_date_to_solr(obj["timestamp"])
@@ -39,6 +41,11 @@ for newspaper in data.keys():
                     entities_memo[entity_id] = parsed
 
             obj["article"]["entities"] = new_entities
+
+            for entity in new_entities:
+                if "slug" in entity: del entity["slug"]
+                if "entity_pk" in entity: del entity["entity_pk"]
+
             output.append(obj)
 
 json.dump(output, output_file, indent=4)
--- m2-information-retrieval/scripts/populate_docker.sh
+++ m2-information-retrieval/scripts/populate_docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+docker exec $(docker ps -lq) bin/solr delete -c arquivo
+docker exec $(docker ps -lq) bin/solr create_core -c arquivo
+
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @../schema.json \
     http://localhost:8983/solr/arquivo/schema
 
+docker cp ../dataset/data.json $(docker ps -lq):/data/arquivo.json
 docker exec $(docker ps -lq) bin/post -c arquivo /data/arquivo.json
