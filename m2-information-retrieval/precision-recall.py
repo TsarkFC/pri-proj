3c7b47775957ed064c2f0f2a830881e9a9163568
xavier <up201806134@fe.up.pt>
2021-12-12 20:33:14 +0000

    Probably final graphs

--- m2-information-retrieval/precision-recall.py
+++ m2-information-retrieval/precision-recall.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from numpy.lib.function_base import percentile
+import matplotlib
 
 # precision = [1, 1, 1, 1, 1, 0.83, 0.86, 0.75, 0.77, 0.8]
 # recall = [0.09, 0.18, 0.27, 0.36, 0.45, 0.45, 0.54, 0.54, 0.63, 0.73]
@@ -7,8 +7,8 @@ from numpy.lib.function_base import percentile
 # precision = [0, 0.5, 0.67, 0.5, 0.6, 0.67, 0.71, 0.75, 0.67, 0.7]
 # recall = [0, 0.09, 0.18, 0.18, 0.27, 0.36, 0.45, 0.54, 0.54, 0.63]
 
-# precision = [1, 1, 1, 0.75, 0.8, 0.83, 0.86, 0.75, 0.77, 0.70]
-# recall = [0.09, 0.18, 0.27, 0.27, 0.36, 0.45, 0.54, 0.54, 0.63, 0.63]
+precision = [1, 1, 1, 0.75, 0.8, 0.83, 0.86, 0.75, 0.77, 0.70]
+recall = [0.09, 0.18, 0.27, 0.27, 0.36, 0.45, 0.54, 0.54, 0.63, 0.63]
 
 # precision = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0.1]
 # recall = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0.1]
@@ -21,16 +21,24 @@ from numpy.lib.function_base import percentile
 
 ticks = [0, 0.2, 0.4, 0.6, 0.8, 1]
 
+plt.rcParams.update({'font.size': 19})
+figures, axis = plt.subplots(1, 2, sharey=True, sharex=True)
+
+axis[0].plot(recall, precision)
+
 # Uncomment for interpolated graphic
-# for i in range(len(precision)):
-#     precision[i] = max(precision[i:])
+for i in range(len(precision)):
+    precision[i] = max(precision[i:])
 
+axis[1].plot(recall, precision)
 
-plt.plot(recall, precision)
-plt.xlabel("Recall")
-plt.ylabel("Precision")
+axis[0].set_xlabel("Recall")
+axis[1].set_xlabel("Recall")
+axis[0].set_ylabel("Precision")
+axis[0].set_title("Precision-Recall Curve")
+axis[1].set_title("Precision-Recall Curve Interpolated")
 plt.xticks(ticks)
 plt.yticks(ticks)
-plt.subplots_adjust(left=0.3, right=0.7, bottom=0.2, top=0.8)
+plt.subplots_adjust(left=0.061, right=0.973, bottom=0.061, top=0.938, hspace=0.2, wspace=0.089)
 
 plt.show()
